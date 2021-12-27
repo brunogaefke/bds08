a9cb0dd59bb77c1cf404cf4ad2e628318ff3b916
Bruno Gaefke <brunogaefke@gmail.com>
2021-12-27 11:55:01 -0300

    Add filter component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Filter from './components/filter';
 import Header from './components/header';
 
 function App() {
   return (
     <>
       <Header />
+      <Filter />
     </>
   );
 }
